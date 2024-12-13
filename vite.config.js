226444e522ae5a66220987b75240c157d404a558
faqhh <mnurfaqih7@gmail.com>
2024-12-13 18:44:28 +0700

    Fix build output directory to 'build'

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',  // Pastikan Vite output-nya ke direktori 'build'
+  },
   resolve: {
-    build: {
-    outDir: 'build',
     alias: {
       '@': '/src',
-    },
     },
   },
 });
